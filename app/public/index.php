6b7697946ffcdb9d3db5459d21be9d985183de6a
JorgeDS <jrivera@piensads.com>
2023-01-11 16:50:03 -0600

    Collector | Limpieza de código

--- app/public/index.php
+++ app/public/index.php
@@ -1,14 +1,16 @@
 <?php
-    /**
-     * CIRACO
-     * @author Jorge Echeverria <snithfferx@outlook.com>
-     * @version 1.0.0
-     */
-    require __DIR__ . '/../../vendor/autoload.php';
-    use app\core\LoaderClass;
-    $app = new LoaderClass;
-    $app->init();
-    $response = $app->verifyRequest();
-    echo $app->display($response);
-    $app->terminate();
-?>
+
+/**
+ * CIRACO
+ * @author Jorge Echeverria <snithfferx@outlook.com>
+ * @version 1.0.0
+ */
+require __DIR__ . '/../../vendor/autoload.php';
+
+use app\core\LoaderClass;
+
+$app = new LoaderClass;
+$app->init();
+$response = $app->verifyRequest();
+$app->display($response);
+$app->terminate();
