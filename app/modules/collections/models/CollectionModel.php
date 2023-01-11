8dfdc245ea77131b926c0663d0bc7b20d26c7d69
JorgeDS <jrivera@piensads.com>
2023-01-11 16:47:53 -0600

    #144 | Collector | Module: Collections | Modelo agregado

--- app/modules/collections/models/CollectionModel.php
+++ app/modules/collections/models/CollectionModel.php
@@ -5,17 +5,26 @@
  * @version 1.0.0
  */
 namespace app\modules\collections\models;
-use app\core\classes\context\ExternalConnection;
-class CollectionModel extends ExternalConnection
+use app\core\classes\context\ExternalContext;
+
+class CollectionModel extends ExternalContext
 {
-    public function get ($value) :array {
-        return $this->getCollections($value);
+    public function get ($values) :array {
+        if (is_string($values) && $values == "all") {
+            $response = $this->getCollections();
+        } else {
+            $response = $this->getCollections($values);
+        }
+        return $response;
     }
 
-    private function getCollections($parameters) {
-        if (!empty($parameters)) {
-            return $this->makeRequest(['type'=>"get",'request'=>['element'=>"collections",'value'=>""]]);
+    private function getCollections(array $parameters = []) :array {
+        if (!empty($parameters['value'])) {
+            $result = (!empty($parameters['fields'])) ? $this->makeRequest("get", "CustomCollection", $parameters['value']) : $this->makeRequest("get", "CustomCollection", $parameters['value'], $parameters['fields']);
+        } else {
+            $result = $this->makeRequest("get", "CustomCollection");
         }
+        return $result;
     }
 }
 ?>
